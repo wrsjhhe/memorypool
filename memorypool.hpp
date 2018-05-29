e4c319cd048396529213c987bc9895c6ea37634e
wrsjhhe <365843679@qq.com>
2018-05-29 13:34:21 +0800

    Update memorypool.hpp

--- memorypool.hpp
+++ memorypool.hpp
@@ -1,5 +1,24 @@
 #pragma once
-
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+// Memorypool                                                                //
+//                                                                           //
+// A structure for memory allocation.                                        //
+//                                                                           //
+// firstblock is the first block of items. nowblock is the block from which  //
+//   items are currently being allocated. nextitem points to the next slab   //
+//   of free memory for an item. deaditemstack is the head of a linked list  //
+//   (stack) of deallocated items that can be recycled.  unallocateditems is //
+//   the number of items that remain to be allocated from nowblock.          //
+//                                                                           //
+// Traversal is the process of walking through the entire list of items, and //
+//   is separate from allocation.  Note that a traversal will visit items on //
+//   the "deaditemstack" stack as well as live items.  pathblock points to   //
+//   the block currently being traversed.  pathitem points to the next item  //
+//   to be traversed.  pathitemsleft is the number of items that remain to   //
+//   be traversed in pathblock.                                              //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
 class memorypool {
 
 public:
