aed37320bf77a17da2b6b1aaf56d56a04793a63a
wrsjhhe <365843679@qq.com>
2018-06-06 15:08:34 +0800

    add function test and bench test

--- test.cpp
+++ test.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <time.h>
 #include <vector>
-int main()
+
+//效率测验
+void bench()
 {
 	memorypool *m = new memorypool(sizeof(int), 4, sizeof(void*), 0);
 
@@ -23,7 +25,7 @@ int main()
 		m->dealloc(p0);
 		m->dealloc(p1);
 		m->dealloc(p2);
-		m->dealloc(p3);	
+		m->dealloc(p3);
 	}
 
 	clock_t end_time = clock();
@@ -59,6 +61,77 @@ int main()
 	}
 	end_time = clock();
 	std::cout << "vector用时:" << end_time - start_time << std::endl;
+
+	delete m;
+}
+
+class People
+{
+public:
+	char name[20];
+	int age;
+
+	People(char* n, int a) :age(a)
+	{
+		strcpy_s(name, n);
+	}
+
+	~People()
+	{
+		std::cout << "destructor" << std::endl;
+	}
+};
+
+
+//功能示例
+void test()
+{
+	//堆上分配内存大小32 * 4 + sizeof(void*) + 8 = 144
+	memorypool *m = new memorypool(30, 4, sizeof(void*), 0);
+
+	//分配内存
+	People* p0 = (People*)m->alloc();
+	People* p1 = (People*)m->alloc();
+	People* p2 = (People*)m->alloc();
+	People* p3 = (People*)m->alloc();
+
+	//添加数据
+	strcpy_s(p0->name, "zhangsan");
+	p0->age = 20;
+
+	strcpy_s(p1->name, "lisi");
+	p1->age = 19;
+
+	strcpy_s(p2->name, "wangwu");
+	p0->age = 21;
+
+	strcpy_s(p3->name, "zhaoliu");
+	p1->age = 18;
+
+	//遍历
+	m->traversalinit();
+	People* px = (People*)m->traverse();
+	px = (People*)m->traverse();
+	px = (People*)m->traverse();
+	m->traversalinit();
+	px = (People*)m->traverse();
+
+
+	//释放内存
+	m->dealloc(p0);
+	m->dealloc(p1);
+	m->dealloc(p2);
+	m->dealloc(p3);
+
+	delete m;
+}
+
+int main()
+{
+	test();
+	bench();
+
 	system("pause");
 	return 0;
 }
+
